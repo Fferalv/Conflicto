6045d62ace377d83aed17c4c5ae3ba0fea7618d8
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-23 09:23:22 +0200

    Coreccion 2.0

--- holamundo.java
+++ holamundo.java
@@ -1,11 +1,6 @@
 public class holamundo {
     public static void main (String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hola mundo en la rama main");
-        System.out.println("Hola mundo de nuevo en la rama main");
-=======
         System.out.println("Hola mundo en la rama login");
         System.out.println("Hola mundo de nuevo en la rama login");
->>>>>>> login
     }
 }
