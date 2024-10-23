9aed18cd7d65c574dc036197a209ee559d5d9573
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-23 09:03:17 +0200

    cambios en la rama main

--- holamundo.java
+++ holamundo.java
@@ -1,6 +1,6 @@
 public class holamundo {
     public static void main (String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Hola mundo de nuevo");
+        System.out.println("Hola mundo en la rama main");
+        System.out.println("Hola mundo de nuevo en la rama main");
     }
 }
