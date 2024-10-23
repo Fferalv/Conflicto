db7e1687df8687d4f146afe0369df7339fce716b
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-23 09:00:34 +0200

    Confirmando cambios en la rama login

--- holamundo.java
+++ holamundo.java
@@ -1,6 +1,6 @@
 public class holamundo {
     public static void main (String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Hola mundo de nuevo");
+        System.out.println("Hola mundo en la rama login");
+        System.out.println("Hola mundo de nuevo en la rama login");
     }
 }
